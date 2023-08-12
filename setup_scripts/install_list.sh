31c4f205df02c07bee451672a709c1a0897435ae
Dharanesh07 <robbiedharanesh@gmail.com>
2023-08-12 22:35:44 +0530

    setup scripts

---
+++ setup_scripts/install_list.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo adding PPA
+
+#sudo add-apt-repository ppa:linuxuprising/apps -y
+#sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+#sudo add-apt-repository multiverse -y 
+#sudo add-apt-repository ppa:diodon-team/stable -y
+#sudo add-apt-repository ppa:aslatter/ppa -y
+
+echo adding PPA completed
+
+sudo apt update
+
+sudo apt install -y \
+rofi \
+git \
+curl \
+tlpui \
+htop \
+grub-customizer \
+neovim \
+meld \
+ttf-mscorefonts-installer \
+diodon \
+vlc \
+adb \
+tmux \
+minicom \
+alacritty \
+fonts-roboto \
+fzf
+
+
